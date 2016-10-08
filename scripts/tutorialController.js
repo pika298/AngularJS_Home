a837445b1c7d5d4b4351fd54b765075e9109b6af
pika298 <kit2008@live.com>
2016-10-08 23:57:41 +0800

    Inserted house info

---
+++ scripts/tutorialController.js
@@ -0,0 +1,27 @@
+angular
+	.module('tutorial_1')
+	.controller('tutorialController', function($scope) {
+
+		$scope.homes = [
+			{
+				"type": "Condo",
+				"price": 220000,
+				"address": "213 Grove Street",
+				"description": "Excellent place, really nice view!"
+			},
+			{
+				"type": "House",
+				"price": 410500,
+				"address": "7823 Winding Way",
+				"description": "Beautiful home with lots of space for a large family."
+			},
+			{
+				"type": "Duplex",
+				"price": 395000,
+				"address": "834 River Lane",
+				"description": "Great neighborhood and lots of nice gree space."
+			}
+
+		];
+
+	});
