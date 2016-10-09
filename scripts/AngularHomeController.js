1b308c6a2f00520ccd448e1c92bcc022178b3f95
pika298 <kit2008@live.com>
2016-10-09 19:15:54 +0800

    Edit Listing

--- scripts/AngularHomeController.js
+++ scripts/AngularHomeController.js
@@ -4,14 +4,33 @@ angular
 
 		$scope.homes;
 
+		$scope.priceInfo = {
+			min: 0,
+			max: 1000000
+		}
+
+		$scope.newListing = {};
+
+		$scope.addHome = function(newListing) {
+			newListing.image = 'default';
+			$scope.homes.push(newListing);
+			$scope.newListing = {};
+		}
+
+		$scope.editHome = function(home) {
+			$scope.editListing = true;
+			$scope.existingListing = home;
+		}
+
+		$scope.saveHomeEdit = function() {
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
+
 		AngularHomeFactory.getAngularHome().success(function(data) {
 			$scope.homes = data;
 		}).error(function(error) {
 			console.log(error);
 		}); 
-
-		$scope.sayHello = function() {
-			console.log("Hello!");
-		}
 
 	});
